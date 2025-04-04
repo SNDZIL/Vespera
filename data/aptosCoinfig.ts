894ba15b8260ee5a223b327e9dab6f6a71dbdf03
刘程鑫 <756004757@qq.com>
2025-04-04 13:39:39 +0800

    fix: pay

--- data/aptosCoinfig.ts
+++ data/aptosCoinfig.ts
@@ -2,12 +2,12 @@ import { Aptos, AptosConfig, Network } from '@aptos-labs/ts-sdk';
  
 const config = new AptosConfig({ network: Network.TESTNET });
 export const aptos = new Aptos(config);
-export const ADDRESS = "0x6e119506e14f076333557500d3d1025b63f757c570abee35b1f9dcb1514cff0a"
+export const ADDRESS = "0x060a67a4474353252bf06989d00e96b55f5d18f44554e6a8323afb4b94ae5e15"
 export const MODULE = "lend"
 
-export const ConfigAddress = "0xbbb23329339d042c543566a9fe3ca5c35e775b5e6518d585e5bfcfae4524ec2e"
-export const CoffeeAddress = "0xf87d46de310897a1a34abab457e7ce3395a3e9a7cff4db0e7ca744440a2d7007"
-export const CoffeeProfile = "0xc30085faedf674462f02e0fda13f6491612d26629e7df9f572f7f9e7f4c22b52"
+export const ConfigAddress = "0xec1d87f61881c45f7252a4c519e8486c1c530571d26245758a78ea8ca662a17e"
+export const CoffeeAddress = "0x376fba087c1103d3378ec9f73cd3302ed56111b2d7721b864985637ad8a5bd23"
+export const CoffeeProfile = "0x54da9d25ff0772524ff18bdba4351ddba425ca4ef7ebcead9bc55878e93bf23"
 
 export const getMacCredit = async(address: string)=>{
   const payload = {
